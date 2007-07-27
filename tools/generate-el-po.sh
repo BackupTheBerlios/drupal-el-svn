207db2bf46bfcb9a9b0966e8320e594c8cdddf05
lourdas_v <lourdas_v@bb7bb442-4931-0410-a6ea-8b61ee413a62>
2007-07-27 18:36:51 +0000

    Various fixes.

--- tools/generate-el-po.sh
+++ tools/generate-el-po.sh
@@ -3,5 +3,8 @@
 # Generates a single po file (el.po) from a folder containing all the small 
 # po files.
 
+mv ../el-po/installer.po ../el-po/installer.po.
+
 msgcat --use-first ../el-po/general.po ../el-po/[^g]*.po | msgattrib --no-fuzzy -o el.po
 
+mv ../el-po/installer.po. ../el-po/installer.po
