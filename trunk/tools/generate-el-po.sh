6dddb46104656e04b599ed62aadc3d516d0a0353
lourdas_v <lourdas_v@bb7bb442-4931-0410-a6ea-8b61ee413a62>
2008-02-19 21:32:17 +0000

    Changed the script to work for the 6.x series.

--- trunk/tools/generate-el-po.sh
+++ trunk/tools/generate-el-po.sh
@@ -3,8 +3,4 @@
 # Generates a single po file (el.po) from a folder containing all the small 
 # po files.
 
-mv ../5.x-el-po/installer.po ../5.x-el-po/installer.po.
-
-msgcat --use-first ../5.x-el-po/general.po ../5.x-el-po/[^g]*.po | msgattrib --no-fuzzy -o el.po
-
-mv ../5.x-el-po/installer.po. ../5.x-el-po/installer.po
+msgcat ../6.x-el-po/*.po | msgattrib --no-fuzzy -o el.po
